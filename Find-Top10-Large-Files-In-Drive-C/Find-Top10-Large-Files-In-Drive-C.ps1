2c4aa97243acfe632b6b087d29016105ebce5b27
celeron633 <dengxionghui@hotmail.com>
2025-07-31 22:16:12 +0800

    ps1: update Find-Top10-Large-Files-In-Drive-C

--- Find-Top10-Large-Files-In-Drive-C/Find-Top10-Large-Files-In-Drive-C.ps1
+++ Find-Top10-Large-Files-In-Drive-C/Find-Top10-Large-Files-In-Drive-C.ps1
@@ -1,4 +1,8 @@
 Get-ChildItem -Recurse "C:\" -File -ErrorAction SilentlyContinue | 
     Where-Object {$_.Length -gt (10 *1024 *1024)} | 
         Sort-Object Length -Descending | 
-            Select-Object FullName, Length -First 10
+            Select-Object FullName, Length -First 10 |
+                ForEach-Object {
+                    $fileSizeMB = $_.Length / 1024 / 1024
+                    [System.Console]::WriteLine("Path: [{0}], Size: [{1:F2}]MB", $_.FullName, $fileSizeMB)
+                }
